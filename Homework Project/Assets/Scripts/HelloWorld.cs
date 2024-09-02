5136c72dd38e3bfd078bb61e4db99e797c57cb03
TravisA01 <travista@udel.edu>
2024-09-02 15:55:35 -0400

    Update HelloWorld.cs

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,16 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     Debug.Log("Hello, world!");   
+        Debug.Log("Hello, world!");
+        Debug.LogWarning("I am a warning");
+        Debug.LogError("This is an error");
+        // Author: Travis Anderson
+        /*
+         *Is there usually this much trial and error in programming?
+         *How much of what we learn is just like this?
+         *Would a more complex game have more complex code?
+         */
+
     }
 
     // Update is called once per frame
